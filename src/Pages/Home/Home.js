4dd9a620fad9954587ce6e8988af2bf3f3babf2e
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2023-01-12 20:59:40 +0600

    firebase

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Contact from './Contact/Contact';
 import ImageGallery from './ImageGallery/ImageGallery';
 
 const Home = () => {
     return (
         <div>
             <ImageGallery></ImageGallery>
+            <Contact></Contact>
         </div>
     );
 };
