1656df524b7d72436decf4ebf00d2e7dcc890a42
shamim-s <shamimibas@gmail.com>
2023-01-13 00:19:13 +0600

    Changed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import Contact from './Contact/Contact';
-<<<<<<< HEAD
-import Hero from './Hero/Hero';
-=======
 import FinancialSolutions from './FinancialSolutions/FinancialSolutions';
->>>>>>> 1b639047d2bf3fb7cba3a9fd091b78c311c662ac
 import ImageGallery from './ImageGallery/ImageGallery';
 
 const Home = () => {
     return (
         <div>
-<<<<<<< HEAD
-            <Hero/>
-=======
             <FinancialSolutions></FinancialSolutions>
->>>>>>> 1b639047d2bf3fb7cba3a9fd091b78c311c662ac
             <ImageGallery></ImageGallery>
             <Contact></Contact>
         </div>
