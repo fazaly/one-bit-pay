1b639047d2bf3fb7cba3a9fd091b78c311c662ac
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2023-01-12 23:40:06 +0600

    financial Card

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Contact from './Contact/Contact';
+import FinancialSolutions from './FinancialSolutions/FinancialSolutions';
 import ImageGallery from './ImageGallery/ImageGallery';
 
 const Home = () => {
     return (
         <div>
+            <FinancialSolutions></FinancialSolutions>
             <ImageGallery></ImageGallery>
             <Contact></Contact>
         </div>
