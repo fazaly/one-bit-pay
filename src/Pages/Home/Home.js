46afd65eac1e0eeec79facadf0eb41a78eeb88c7
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2023-01-13 00:27:58 +0600

    hero done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Contact from './Contact/Contact';
 import FinancialSolutions from './FinancialSolutions/FinancialSolutions';
+import Hero from './Hero/Hero';
 import ImageGallery from './ImageGallery/ImageGallery';
 
 const Home = () => {
     return (
         <div>
+            <Hero></Hero>
             <FinancialSolutions></FinancialSolutions>
             <ImageGallery></ImageGallery>
             <Contact></Contact>
