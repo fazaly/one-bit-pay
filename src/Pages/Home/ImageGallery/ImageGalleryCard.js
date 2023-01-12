b627bcc8419634bef4a1f687bcd4c164c5ff37f1
AhmadShopnil <ahmadshopnil75@gmail.com>
2023-01-12 12:55:10 +0600

    added image gallery

---
+++ src/Pages/Home/ImageGallery/ImageGalleryCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ImageGalleryCard.css'
+const ImageGalleryCard = ({ imageInfo }) => {
+    console.log(imageInfo)
+    return (
+        <>
+            <div style={{
+                transition: '.5s'
+            }} className="rounded-lg hoverEffect w-full hover:w-full cursor-pointer ">
+                <figure><img className='rounded-xl w-full md:w-full h-full' src={imageInfo.Img} alt="Shoes" /></figure>
+            </div>
+
+
+
+            {/* <div className="md:hidden w-full bg-base-100 shadow-xl">
+                <figure><img className='w-full md:w-full h-full' src={imageInfo.Img} alt="Shoes" /></figure>
+            </div> */}
+        </>
+
+
+
+    );
+};
+
+export default ImageGalleryCard;
