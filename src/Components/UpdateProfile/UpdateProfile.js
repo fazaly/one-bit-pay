5afe9509dbf7eedf743adaeb2a0f8f2c4243c0d0
shamim-s <shamimibas@gmail.com>
2023-01-19 21:01:02 +0600

    added responsiveness in update profile and added a loader

--- src/Components/UpdateProfile/UpdateProfile.js
+++ src/Components/UpdateProfile/UpdateProfile.js
@@ -1,102 +1,123 @@
-import React from 'react';
+import React from "react";
 
 const UpdateProfile = () => {
-    return (
-        <div>
+  return (
+    <div className="w-full flex bg-white rounded-lg shadow-xl lg:mt-5 mt-16 mb-5">
+      <div className="lg:w-32 w-10 min-h-full bg-[#5966FF]">
+        <h1 className="text-[#5966FF]"></h1>
+      </div>
 
-            <div className=' shadow-2xl bg-base-100 grid grid-cols-7 my-6 mx-20 rounded-2xl'>
-
-                <div className='mx-10 mb-10 mt-3  col-span-6 '>
-
-                    <div className='mb-8  flex justify-between'>
-                        <h3 className='text-xl font-bold mt-14 '>Update Your Information</h3>
-                        <h3 className='text-2xl justify-end font-bold'>OneBitPay</h3>
-                    </div>
-                    <div className='grid grid-cols-2  gap-14 '>
-
-                        <div className=' grid grid-cols-1  gap-1'>
-
-                            <div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full " />
-                                <div className="divider"></div>
-                            </div><div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full " />
-                                <div className="divider"></div>
-                            </div><div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full " />
-                                <div className="divider"></div>
-                            </div>
-                            <div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full" />
-                                <div className="divider"></div>
-                            </div>
-
-                        </div>
-                        <div className=' grid grid-cols-1 gap-1'>
-
-                            <div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full " />
-                                <div className="divider"></div>
-                            </div><div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full " />
-                                <div className="divider"></div>
-                            </div><div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full " />
-                                <div className="divider"></div>
-                            </div>
-                            <div className="form-control w-full ">
-                                <label className="label">
-                                    <span className="label-text"> Your first name</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input  w-full " />
-                                <div className="divider"></div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className='flex justify-between'>
-
-                        <button className="btn btn-outline btn-primary rounded-full">Clear</button>
-                        <button className="btn  btn-primary rounded-full">Update</button>
-
-
-                    </div>
-
-                </div>
-
-                <div style={{
-                    background: '#5966FF'
-                }} className=' h-full  rounded-l-2xl'>
-                    {/* Left Side */}
-
-                </div>
-
+      <div className="p-4">
+        {/* Input fields */}
+        <h1 className="text-2xl font-semibold uppercase text-[#5966FF]">
+          Update Your Informations
+        </h1>
+        <div className="grid lg:grid-cols-2 gap-6 mx-auto mt-5">
+          <div className="lg:w-96 w-72">
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input  w-full "
+              />
+              <div className="divider mt-0 mb-0"></div>
             </div>
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input  w-full "
+              />
+              <div className="divider mt-0 mb-0"></div>
+            </div>
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input  w-full "
+              />
+              <div className="divider mt-0 mb-0"></div>
+            </div>
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input  w-full"
+              />
+              <div className="divider mt-0 mb-0"></div>
+            </div>
+          </div>
 
+          <div className="lg:w-96 w-72">
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input  w-full "
+              />
+              <div className="divider mt-0 mb-0"></div>
+            </div>
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input  w-full "
+              />
+              <div className="divider mt-0 mb-0"></div>
+            </div>
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input w-full "
+              />
+              <div className="divider mt-0 mb-0"></div>
+            </div>
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text"> Your first name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input w-full "
+              />
+              <div className="divider mt-0 mb-0"></div>
+            </div>
+          </div>
         </div>
-    );
+        <div className="flex justify-between items-center mt-5">
+          <button className="btn bg-transparent hover:bg-transparent hover:text-[#939cff] text-[#5966FF] border-none btn-xs">
+            Reset All
+          </button>
+          <button className="btn bg-[#5966FF] rounded-full border-none">
+            confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateProfile;
--- src/Dashbord/MobileRecharge/MobileRecharge.js
+++ src/Dashbord/MobileRecharge/MobileRecharge.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Spinner from '../../Components/Spinner/Spinner';
 
 const MobileRecharge = () => {
     return (
-        <div>
-            <h2 className='text-2xl font-bold text-center'>Mobile Recharge</h2>
+        <div className="flex flex-col justify-center items-center w-full h-full">
+        <h2 className="text-center text-3xl font-bold uppercase">
+         Mobile Recharge comming
+        </h2>
+        <div className="flex items-center gap-2">
+          <span className="text-center lg:text-9xl text-7xl font-normal uppercase text-[#5966FF]">
+            s
+          </span>
+          <Spinner />
+          <Spinner />
+          <span className="text-center lg:text-9xl text-7xl font-normal uppercase text-[#5966FF]">
+            n
+          </span>
         </div>
+      </div>
     );
 };
 
