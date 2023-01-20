85657bab85ea046c057c98dd6552bd3335399956
shamim-s <shamimibas@gmail.com>
2023-01-20 17:09:41 +0600

    fixed some error

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -37,7 +37,7 @@ const AuthProvider = ({children}) => {
             setLoading(false);
         });
         return () => unsubscribe();
-    }, []);
+    }, [user]);
 
     const authInfo = {
         user,
