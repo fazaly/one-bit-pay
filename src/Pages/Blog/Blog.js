74b382117ebafdf44d3b2364923fd180750c703c
jaber1104076 <jabercuet11@gmail.com>
2023-01-26 23:14:22 +0600

    blog added

---
+++ src/Pages/Blog/Blog.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import BlogInfo from './BlogInfo';
+
+const Blog = () => {
+
+    const [blogpost, setBlogPost] = useState([])
+
+    useEffect(() => {
+        fetch('https://one-bit-pay-server.vercel.app/blogs')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setBlogPost(data)
+            })
+    }, [])
+    return (
+        <div>
+            <section className="pt-10 lg:pt-[80px] pb-10 lg:pb-20">
+                <div className="container">
+                    <div className="flex flex-wrap justify-center -mx-4">
+                        <div className="w-full px-4">
+                            <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
+                                <span className="font-semibold text-lg text-primary mb-2 block">
+                                    Our Blogs
+                                </span>
+                                <h2
+                                    className="
+                  font-bold
+                  text-3xl
+                  sm:text-4xl
+                  md:text-[40px]
+                  text-dark
+                  mb-4
+                  "
+                                >
+                                    Our Recent News
+                                </h2>
+                                <p className="text-base text-body-color">
+                                    Finential sector have been focused on MFS recent time from them mobile banking is now most popular.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-2">
+                        {
+                            blogpost.map(blog => <BlogInfo
+                                key={blog._id}
+                                blog={blog}
+                            ></BlogInfo>)
+                        }
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Blog;
