027a287ff2353f1fa4397567418ee66bf27c28a1
fazaly <fazalyrabbi614@gmail.com>
2023-01-27 16:32:29 +0600

    bill pay

--- src/Dashbord/BillPay/BillCard.js
+++ src/Dashbord/BillPay/BillCard.js
@@ -3,12 +3,11 @@ import React from 'react';
 const BillCard = ({ bill }) => {
     return (
         <div className="">
-            <div className="card lg:w-80 w-96 bg-white text-primary-content mx-auto shadow-lg">
+            <div className="card w-32 bg-white text-primary-content mx-auto shadow-2xl">
                 <div className="py-6 flex justify-center gap-3">
-                    <h1 className="font-bold text-xl text-[#5966FF] opacity-50">
+                    <h1 className="font-bold text-xl text-center text-[#5966FF] opacity-50">
                         {bill.name}
                     </h1>
-                    <button className="btn btn-outline btn-info btn-xs mt-1">Go</button>
                 </div>
             </div>
         </div>
