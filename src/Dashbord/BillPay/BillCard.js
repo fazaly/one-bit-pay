0443a2c17f9a33124b3ad26ae172b1581285c63d
fazaly <fazalyrabbi614@gmail.com>
2023-01-27 16:46:36 +0600

    bill section Updated

--- src/Dashbord/BillPay/BillCard.js
+++ src/Dashbord/BillPay/BillCard.js
@@ -3,9 +3,9 @@ import React from 'react';
 const BillCard = ({ bill }) => {
     return (
         <div className="">
-            <div className="card w-32 bg-white text-primary-content mx-auto shadow-2xl">
+            <div className="card w-32 bg-white text-primary-content mx-auto shadow-lg">
                 <div className="py-6 flex justify-center gap-3">
-                    <h1 className="font-bold text-xl text-center text-[#5966FF] opacity-50">
+                    <h1 className="font-bold text-xl text-center text-gray-900 opacity-50">
                         {bill.name}
                     </h1>
                 </div>
--- src/Dashbord/BillPay/BillPay.js
+++ src/Dashbord/BillPay/BillPay.js
@@ -63,7 +63,8 @@ const BillPay = () => {
                 />
             <div className="divider mt-0 mb-0"></div>
             </div>
-            <h3 className='text-2xl font-bold mb-8 mt-8'>Bill Type</h3>
+            <button className='btn text-2xl capitalize bg-[#5966FF] rounded border-none'>Pay</button>
+            <h3 className='text-2xl font-bold mb-6 mt-8'>Bill Type</h3>
             <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-10'>
                 {
                     AllBils?.map(bill => <BillCard key={bill.id} bill={bill}></BillCard>)
