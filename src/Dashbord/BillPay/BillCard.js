0b82374f58f9f2b9c6f0ecbbc4588ed94dc1a50b
fazaly <fazalyrabbi614@gmail.com>
2023-02-01 00:12:18 +0600

    bill pay

--- src/Dashbord/BillPay/BillCard.js
+++ src/Dashbord/BillPay/BillCard.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 const BillCard = ({ bill }) => {
     return (
-        <div>
-            <div>
-                <div>
-                    <button type='submit' className='btn btn-lg btn-outline capitalize font-bold text-xl text-gray-900 opacity-50'>
-                        {bill.name}
-                    </button>
-                </div>
-            </div>
+        <div className='min-w-'>
+            <button type='submit' className='btn w-full btn-outline capitalize font-bold text-xl text-gray-900 opacity-50'>
+                <span>{bill.icon}</span>
+                <span>{bill.name}</span>
+            </button>
         </div>
     );
 };
