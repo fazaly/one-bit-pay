9c7d2f6156cab145ad3f9fdcd93b521a0ae0ace1
shamim-s <shamimibas@gmail.com>
2023-02-28 16:02:57 +0600

    Changed mobile recharge page design

--- src/Dashbord/Withdraw/Withdraw2.js
+++ src/Dashbord/Withdraw/Withdraw2.js
@@ -49,21 +49,59 @@ const Withdraw2 = () => {
     };
 
     withdraw(WithdrawInfo);
+
+    setTimeout(() => {
+      form.reset();
+    }, 3000);
+    
   };
 
   return (
-    <div className="pt-10">
-      <div className="">
-          <div className="bg-[#181818] p-8 rounded-lg">
-               <h1 className="text-3xl font-bold text-white mb-4">Your Balance</h1>
-               <div>
-                    <h1 className="text-5xl font-semibold text-white"><span className="text-[#5966FF]">$</span>{userDetails?.balance}.00</h1>
-               </div>     
-          </div>          
+    <div className="pt-10 p-6">
+      <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6">
+        <div className="bg-[#181818] p-8 rounded-lg">
+          <h1 className="text-3xl font-bold text-white mb-4">Your Balance</h1>
+          <div>
+            <h1 className="text-5xl font-semibold text-white">
+              <span className="text-[#5966FF]">$</span>
+              {userDetails?.balance}.00
+            </h1>
+          </div>
+        </div>
+
+        <div className="">
+          <h1 className="font-bold text-xl text-[#181818] mb-4">
+            Withdraw Amount
+          </h1>
+          <form onSubmit={handleWithdraw} className="space-y-6">
+            <input
+              type="text"
+              name="receiverEmail"
+              required
+              placeholder="agentEmail"
+              className="input input-bordered w-full text-slate-700 focus:text-[#5966FF] focus:border-b-[#5966FF]"
+            />
+            <input
+              type="text"
+              name="amount"
+              required
+              placeholder="Amount"
+              className="input input-bordered w-full text-slate-700 focus:text-[#5966FF]  focus:border-b-[#5966FF]"
+            />
+            <p className="">
+              <button
+                type="submit"
+                className="btn bg-[#181818] w-full  rounded-lg border-none hover:bg-[#5966FF]"
+              >
+                {isLoading ? <ButtonSpinner /> : "Withdraw Now"}
+              </button>
+            </p>
+          </form>
+        </div>
       </div>
 
       <div className="mt-4">
-        <div className="card bg-white text-primary-content shadow-lg">
+        <div className="card bg-white text-primary-content">
           <div>
             <h1 className="font-bold text-xl text-[#5966FF] opacity-100 p-6">
               History
